460648b35a705be61cfe8c987acd7c1cb5a4ca2a
vanraak <129201527+vanraak@users.noreply.github.com>
2025-07-28 14:53:05 +0200

    Added an example financial dataset
    Financial data + load function

--- pychemist/__init__.py
+++ pychemist/__init__.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import warnings
 from scipy.stats import ttest_ind
+import importlib.resources
+
+datasets = [
+    "financials"
+]
+
+def load(name: str) -> pd.DataFrame:
+    if name in datasets:
+        with importlib.resources.files("dsf.data").joinpath(f"{name}.parquet").open("rb") as f:
+            return pd.read_parquet(f)
+    else:
+        raise ValueError(f"Dataset '{name}' does not exist.")
 
 def time_shift(dataframe, variables, id, time, shift=1):
     """
